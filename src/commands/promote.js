e1b58958784096f9cbe463d088be9eb1e4b333fe
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2016-09-08 00:27:32 +0200

    Promote command

--- src/commands/promote.js
+++ src/commands/promote.js
@@ -1,11 +1,28 @@
+const shell = require('shelljs');
+
 function checkConditions() {
   return true;
 }
 
 module.exports = (opts) => {
+  const engine = opts.engine;
+  const options = opts.options;
+  const config = opts.config;
+  const hp4tDir = opts.hp4tDir;
+  const projectDir = opts.projectDir;
+  const logger = opts.logger;
+  const environment = opts.environment;
+
+  function getAppName() {
+    return config.get(environment, 'app');
+  }
+
   function execute() {
-    console.log('Promote');
-    return true;
+    console.log('Promote Heroku application');
+
+    const appName = getAppName();
+
+    return shell.exec(`heroku pipelines:promote --app ${appName}`).code === 0;
   }
 
   const exports = {
