f767471353856bf689588dc624b17dab7733eae3
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2016-09-07 21:33:51 +0200

    Improvements in commands: init Install lodash in init command

--- src/commands/init.js
+++ src/commands/init.js
@@ -11,8 +11,9 @@ module.exports = (opts) => {
   const hp4tDir = opts.hp4tDir;
   const projectDir = opts.projectDir;
   const logger = opts.logger;
-
   const language = options.language;
+  const templatesDir = `${hp4tDir}/src/templates`;
+  const languageTemplatesDir = `${templatesDir}/${language}`;
 
   function backupExistingFiles() {
     shell.ls('-RA', `${hp4tDir}/src/templates/${language}/`).forEach(file => {
@@ -24,25 +25,33 @@ module.exports = (opts) => {
   }
 
   function copyTemplateFiles() {
-    const templateDir = `${hp4tDir}/src/templates/${language}/`;
-    shell.ls('-RA', templateDir).forEach(file => {
-      if( fs.statSync(`${templateDir}/${file}`).isDirectory() ) {
+    shell.ls('-RA', languageTemplatesDir).forEach(file => {
+      if (fs.statSync(`${languageTemplatesDir}/${file}`).isDirectory()) {
         logger.debug(`Create directory: ${file}`);
         shell.mkdir(`${projectDir}/${file}`);
       } else {
-        logger.info(`Copy file: ${path.relative(projectDir, `${templateDir}/${file}`)} => ${file}`);
-        shell.cp(`${templateDir}/${file}`, `${projectDir}/${file}`);
+        logger.info(`Copy file: ${path.relative(projectDir, `${languageTemplatesDir}/${file}`)} => ${file}`);
+        shell.cp(`${languageTemplatesDir}/${file}`, `${projectDir}/${file}`);
       }
     });
   }
 
+  function installLodash() {
+    if (!fs.existsSync(`${projectDir}/package.json`)) {
+      shell.cp(`${templatesDir}/package.json`, `${projectDir}/`);
+    }
+    logger.info('Installing lodash ...');
+    shell.exec('npm i lodash --save');
+  }
+
   function execute() {
-    logger.info('Initialize ${language} project ...');
+    logger.info(`Initialize ${language} project ...`);
 
     backupExistingFiles();
     copyTemplateFiles();
+    installLodash();
 
-    logger.info('Projtect initialized.');
+    logger.info('Project initialized.');
 
     return true;
   }
