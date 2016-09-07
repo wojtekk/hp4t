b5a1011f020de8d04c86c9ca7677df432a397bb5
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2016-09-07 19:10:29 +0200

    Export command

--- src/commands/export.js
+++ src/commands/export.js
@@ -1,11 +1,34 @@
+const heroin = require('heroin-js');
+
 function checkConditions() {
   return false;
 }
 
 module.exports = (opts) => {
+  const options = opts.options;
+  const logger = opts.logger;
+
+  function getHerokuApiKey() {
+    return options.key;
+  }
+
+  function getAppName() {
+    return options.application;
+  }
+
   function execute() {
-    console.log('Export');
-    return true;
+    console.log('Export Heroku application configuration');
+
+    const herokuApiKey = getHerokuApiKey();
+    const appName = getAppName();
+    const heroku = heroin(herokuApiKey, { debug: false });
+
+    return heroku.export(appName)
+      .then(result => {
+        logger.log('Application configuration:\n', result);
+
+        return true;
+      });
   }
 
   const exports = {
